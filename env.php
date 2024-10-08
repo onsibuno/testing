48297db16bdb7bbaa660a32c269962f457915f98
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2024-10-08 15:04:22 +0200

    Projet étape 2: Cypress téléchargé

--- env.php
+++ env.php
@@ -1,6 +1,5 @@
 <?php
-// TODO: Penser à changer les valeurs des variables pour les adapter à votre environnement (en retirant les accolades)
 $host = 'localhost';
-$database = '{votre_base_de_donnees}';
-$login = '{votre_login}';
-$password = '{votre_mot_de_passe}';
+$database = 'github_testing';
+$login = 'marceau';
+$password = 'adrar';
