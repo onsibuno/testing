4f111678e038a5f0d309f3bfd2c72d47ed362765
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2024-10-15 16:06:41 +0200

    Enregistre l'action en BDD de l'issue du test

--- cypress/e2e/formulaire.cy.js
+++ cypress/e2e/formulaire.cy.js
@@ -1,11 +1,30 @@
-describe('Test formulaire d\'ajout', () => {
-  it('passes', () => {
+describe('register', () => {
+  it('addUser', () => {
     cy.visit('http://localhost/github_testing/addUser')
     cy.get('input[name="nom"]').type('DOE')
     cy.get('input[name="prenom"]').type('John')
     cy.get('input[name="mail"]').type(Math.random().toString(36).substring(2, 15) + '@gmail.com')
     cy.get('input[name="mdp"]').type('!P4sSw0rD!')
     cy.get('input[type="submit"]').click()
-    cy.get('#msgzone').should('contain', "Le compte a été ajouté en BDD")
+    cy.get('#msgzone').invoke("text").then((text => {
+      //paramétre JSON (nom du test, date, statut)
+      const name = 'addUser';
+      let date = new Date()
+      date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+      const url = "http://localhost/github_testing/api/addTest"
+      let json = '';
+      if (text == "Le compte a été ajouté en BDD") {
+        const valid = true
+        json = JSON.stringify({ name: name, valid: valid, date: date })
+      } else if (text == "Les informations sont incorrectes") {
+        const valid = false
+        json = JSON.stringify({ name: name, valid: valid, date: date })
+      }
+      cy.request({
+        method: 'POST',
+        url: url,
+        body: json,
+      });
+    }))
   })
 })
